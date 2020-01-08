cb2b4697ffaa3a7b76583728cab7aef2c2b3ba10
Austin Tooley <tooley7@gmail.com>
2020-01-07 20:14:18 -0600

    Created followerList cell

---
+++ gitHubFollowers/Views/GFAvatarImageView.swift
@@ -0,0 +1,33 @@
+//
+//  GFAvatarImageView.swift
+//  gitHubFollowers
+//
+//  Created by Austin Tooley on 1/7/20.
+//  Copyright © 2020 Austin Tooley. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    // MARK: - Properties
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Internal
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
