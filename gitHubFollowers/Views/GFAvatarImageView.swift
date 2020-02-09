dbceb81882132812721a643f6daaf70e16c36c9f
Austin Tooley <tooley7@gmail.com>
2020-02-09 15:10:42 -0600

    Avatar cell image downloading and caching

--- gitHubFollowers/Views/GFAvatarImageView.swift
+++ gitHubFollowers/Views/GFAvatarImageView.swift
@@ -11,6 +11,7 @@ import UIKit
 class GFAvatarImageView: UIImageView {
     
     // MARK: - Properties
+    let cache = NetworkManager.shared.imageCache
     let placeholderImage = UIImage(named: "avatar-placeholder")!
 
     // MARK: - Initializers
@@ -29,5 +30,37 @@ class GFAvatarImageView: UIImageView {
         clipsToBounds = true
         image = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func downloadImage(from urlString: String) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        // Check if we already have the image cached
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = image
+            return
+        }
+        
+        // We don't have the image cached, let's download it
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            // Make sure we're good
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            // Grab the image data
+            guard let image = UIImage(data: data) else { return }
+            
+            // Cache the image
+            self?.cache.setObject(image, forKey: cacheKey)
+            
+            // Set the image
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+        task.resume()
     }
 }
