6b1dece9e839794b4b69d62adc417bfc5ee67172
Austin Tooley <tooley7@gmail.com>
2020-02-13 14:55:35 -0600

    refactored UserInfoVC

--- gitHubFollowers/Controllers/UserInfoViewController.swift
+++ gitHubFollowers/Controllers/UserInfoViewController.swift
@@ -11,18 +11,74 @@ import UIKit
 class UserInfoViewController: UIViewController {
     
     // MARK: - Properties
-    let headerView = UIView()
-    
     var username: String!
+    
+    // UI
+    let headerView = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+    var itemViews = [UIView]()
 
     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+        layoutUI()
+        getUserInfo()
+    }
+    
+    // MARK: - Configure
+    
+    func configure() {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        layoutUI()
+    }
+    
+    func layoutUI() {
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
         
+        itemViews = [headerView, itemViewOne, itemViewTwo]
+        
+        // Set up common parameters
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+        }
+        
+        itemViewOne.backgroundColor = .systemPink
+        itemViewTwo.backgroundColor = .systemBlue
+        
+        // Add remaining unique contraints
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight)
+        ])
+    }
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+    }
+    
+    // MARK: - Internal
+    
+    func getUserInfo() {
         NetworkManager.shared.getUserInfo(for: username) { [weak self] (result) in
             guard let self = self else { return }
             
@@ -36,27 +92,6 @@ class UserInfoViewController: UIViewController {
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.localizedDescription, buttonTitle: "Ok")
             }
         }
-    }
-    
-    // MARK: - Internal
-    
-    func layoutUI() {
-        view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
-        ])
-    }
-    
-    func add(childVC: UIViewController, to containerView: UIView) {
-        addChild(childVC)
-        containerView.addSubview(childVC.view)
-        childVC.view.frame = containerView.bounds
-        childVC.didMove(toParent: self)
     }
     
     @objc
