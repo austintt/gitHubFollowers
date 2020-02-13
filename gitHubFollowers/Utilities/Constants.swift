82104d4e05b43a5c50c35ea72f5ed991506f9672
Austin Tooley <tooley7@gmail.com>
2020-02-13 14:39:51 -0600

    Working on UserInfoVC. HeaderView for user is complete.

---
+++ gitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  gitHubFollowers
+//
+//  Created by Austin Tooley on 2/13/20.
+//  Copyright © 2020 Austin Tooley. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
