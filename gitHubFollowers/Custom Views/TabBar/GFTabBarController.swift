bb2e1c6b4fda9eb693135c368d4699f3425b282c
Austin Tooley <tooley7@gmail.com>
2020-03-04 15:04:24 -0600

    Refactored sceneDelegate, extracting tabBar customization to its own class.

---
+++ gitHubFollowers/Custom Views/TabBar/GFTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  GFTabBarController.swift
+//  gitHubFollowers
+//
+//  Created by Austin Tooley on 3/4/20.
+//  Copyright © 2020 Austin Tooley. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchViewController()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoriteVC = FavoritesListViewController()
+        favoriteVC.title = "Favorites"
+        favoriteVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favoriteVC)
+    }
+}
